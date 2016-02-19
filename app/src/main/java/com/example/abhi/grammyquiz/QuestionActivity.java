e41329d4e1b82f5621a93913a46354ed66047893
aksoni <aksoni@wisc.edu>
2016-02-18 23:21:01 -0600

    Added anwer checking logic and results/answers to results activity

--- app/src/main/java/com/example/abhi/grammyquiz/QuestionActivity.java
+++ app/src/main/java/com/example/abhi/grammyquiz/QuestionActivity.java
@@ -9,8 +9,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class QuestionActivity extends AppCompatActivity {
+
+    static String result1 = null;
+    static String result2 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +32,73 @@ public class QuestionActivity extends AppCompatActivity {
 
     }
 
-    public void onClickNext (View view)
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.paul_simon:
+                if (checked)
+                    result2 = "Incorrect. The correct answer was Fleetwood Mac.";
+                    break;
+            case R.id.marvin_gaye:
+                if (checked)
+                    result2 = "Incorrect. The correct answer was Fleetwood Mac.";
+                    break;
+            case R.id.pink_floyd:
+                if (checked)
+                    result2 = "Incorrect. The correct answer was Fleetwood Mac.";
+                    break;
+            case R.id.fleetwood:
+                if (checked)
+                    result2 = "Correct!";
+                    break;
+        }
+    }
+
+    public void onClickSubmit (View view)
     {
+        EditText editText = (EditText) findViewById(R.id.year_submission);
+        String message = editText.getText().toString();
+
+        if(message.equals("1974")==true)
+        {
+            result1 = "Correct!";
+        }
+        else
+        {
+            result1 = "Incorrect. The correct answer was 1974.";
+        }
+
         getFragmentManager().popBackStack();
         getFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragment_container, TextQuestionFragment.newInstance())
                 .addToBackStack(null)
                 .commit();
+
+
+    }
+
+    public void onClickFinish (View view)
+    {
+        Intent intent = new Intent(this, ResultsActivity.class);
+        startActivity(intent);
     }
 
     Intent intent = getIntent();
+
+    public static String getResult1()
+    {
+        return result1;
+    }
+
+    public static String getResult2()
+    {
+        return result2;
+    }
+
 
 
 }
---
+++ app/src/main/java/com/example/abhi/grammyquiz/ResultsActivity.java
@@ -0,0 +1,38 @@
+package com.example.abhi.grammyquiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+        LinearLayout layout=(LinearLayout)findViewById(R.id.answers);
+        TextView result1 = new TextView(this);
+        result1.setText("Question 1: " + QuestionActivity.getResult1());
+        layout.addView(result1);
+        TextView result2 = new TextView(this);
+        result2.setText("Question 2: " + QuestionActivity.getResult2());
+        layout.addView(result2);
+    }
+
+    public void onClickRestart (View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickQuit (View view)
+    {
+        Intent intent = new Intent(this, ResultsActivity.class);
+        startActivity(intent);
+    }
+}
