3c47f57c4c8c2f181a946930094fb3eed44ca707
aksoni <aksoni@wisc.edu>
2016-02-19 11:12:58 -0600

    Added answer checking logic and results/answers to results activity

--- app/src/main/java/com/example/abhi/grammyquiz/ResultsActivity.java
+++ app/src/main/java/com/example/abhi/grammyquiz/ResultsActivity.java
@@ -26,13 +26,14 @@ public class ResultsActivity extends AppCompatActivity {
 
     public void onClickRestart (View view)
     {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        Intent i = getBaseContext().getPackageManager()
+                .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
     }
 
     public void onClickQuit (View view)
     {
-        Intent intent = new Intent(this, ResultsActivity.class);
-        startActivity(intent);
+        this.finishAffinity();
     }
 }
